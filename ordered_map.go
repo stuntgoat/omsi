5d1e2181a035f89010a72a04f000b33995de9e32
Isaac Gonzalez <isaac@overttone.com>
2013-10-16 01:32:16 -0700

    simplify ordered dict

--- ordered_map.go
+++ ordered_map.go
@@ -4,62 +4,107 @@ import (
 	"fmt"
 )
 
-
+// Link holds the information for a doubly linked list
+// which holds the key as a string, the
+type Link struct {
+	value string
+	previous *Link
+	next *Link
+}
 
 type OrderedMap struct {
 	// maps a key to a value
-	Map map[string]string
+	Map map[string]*Link
 
-	// maps a key to an index in the KeyOrderArray
-	KeyOrderMap map[string]int
-
-	// the order of keys added to Map
-	KeyOrderArray []string
-	count int
+	startLink *Link
+	endLink *Link
 }
 
 func New() *OrderedMap {
 	om := OrderedMap{
-		Map: make(map[string]string),
-		KeyOrderMap: make(map[string]int),
-		KeyOrderArray: make([]string, 0),
+		Map: make(map[string]*Link),
 	}
 	return &om
 }
 
 // Set sets the `key` to the `value`.
 func (om *OrderedMap) Set(key, value string) {
-	_, ok := om.Map[key]
+	retrievedLink, ok := om.Map[key]
 
-	// add non-existent key
+	// add non existent key to map
 	if !ok {
-		om.KeyOrderMap[key] = om.count
-		om.KeyOrderArray = append(om.KeyOrderArray, key)
-		om.count += 1
-	}
-	om.Map[key] = value
+		link := &Link{
+			value: value,
+		}
 
-}
+		// first item
+		if len(om.Map) == 0 {
+			om.startLink = link
+			om.endLink = link
+		} else {
+			// get the most recent 'endLink'
+			lastEnd := om.endLink
 
-// Get retrieves the value from om.Map if it exists
-func (om *OrderedMap) Get(key string) (string, bool) {
-	val, exists := om.Map[key]
-	return val, exists
-}
+			// set this link's 'previous' link as most recent 'endLink'
+			link.previous = lastEnd
 
-// Delete removes a key/value from the OrderedMap. If it does not exist
-// in the map it is a no-op.
-func (om *OrderedMap) Delete(key string) {
-	i, ok := om.KeyOrderMap[key]
-	if !ok {
+			// set this new link as the 'next' link for
+			// the om.endLink
+			lastEnd.next = link
+
+			// set this new link as the end
+			om.endLink = link
+		}
+		om.Map[key] = link
 		return
 	}
-	om.KeyOrderArray[i] = ""
+	retrievedLink.value = value
+}
 
-	delete(om.KeyOrderMap, key)
+// Get returns the value of the link found by the key
+func (om *OrderedMap) Get(key string) (string, bool) {
+	retrievedLink, exists := om.Map[key]
+	if !exists {
+		return "", false
+	}
+
+	return retrievedLink.value, true
+}
+
+// Delete removes a key/value pair if it exists. If the key does not exist
+// the operation is a no-op.
+func (om *OrderedMap) Delete(key string) {
+	retrievedLink, exists := om.Map[key]
+	if !exists {
+		return
+	}
+
+	previous := retrievedLink.previous
+	next := retrievedLink.next
+
+	// if we are deleting the beginning node
+	if retrievedLink == om.startLink {
+
+		// set the next node to the startLink
+		om.startLink = next
+	}
+
+	if previous != nil {
+		previous.next = next
+	}
+
+	if retrievedLink == om.endLink {
+		om.endLink = previous
+	}
+
+	if next != nil {
+		next.previous = previous
+	}
 
 	delete(om.Map, key)
 }
+
+
 
 func main() {
 	o := New()
@@ -68,24 +113,50 @@ func main() {
 	o.Set("dog", "doggy")
 	o.Set("seal", "nails")
 
-	val, ok := o.Get("cat")
-	fmt.Println("val", val)
-	fmt.Println("ok", ok)
+	fmt.Printf("%+v\n", o)
 
-	val, ok = o.Get("dog")
+	val, _ := o.Get("cat")
 	fmt.Println("val", val)
-	fmt.Println("ok", ok)
+	val, _ = o.Get("dog")
+	fmt.Println("val", val)
+	val, _ = o.Get("seal")
+	fmt.Println("val", val)
 
-	val, ok = o.Get("whale")
-	fmt.Println("val", val)
-	fmt.Println("ok", ok)
+	o.Delete("seal")
+	fmt.Printf("%+v\n", o)
 
 	o.Delete("cat")
-	o.Delete("dog")
+	fmt.Printf("%+v\n", o)
 
-	o.Set("chillins", "beer")
-	o.Set("oxygen", "ladies")
-	o.Delete("seal")
+	o.Delete("dog")
+	fmt.Printf("%+v\n", o)
+
+	o.Set("cat", "kitty")
+	o.Set("dog", "doggy")
+	o.Set("seal", "nails")
 
 	fmt.Printf("%+v\n", o)
+
+	o.Delete("cat")
+	fmt.Printf("%+v\n", o)
+
+	o.Delete("seal")
+	fmt.Printf("%+v\n", o)
+
+	o.Delete("dog")
+	fmt.Printf("%+v\n", o)
+
+	o.Set("cat", "kitty")
+	o.Set("dog", "doggy")
+	o.Set("seal", "nails")
+
+	o.Delete("dog")
+	fmt.Printf("%+v\n", o)
+
+	o.Delete("cat")
+	fmt.Printf("%+v\n", o)
+
+	o.Delete("seal")
+	fmt.Printf("%+v\n", o)
+
 }
